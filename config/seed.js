30bdfe3ccded9f7f51f325ef3872b7cda5ceb413
Alex <alex-admin@Ubuntu.myguest.virtualbox.org>
2023-08-21 23:56:43 -0400

    Defeated by relative paths

--- config/seed.js
+++ config/seed.js
@@ -14,10 +14,10 @@ const Item = require('../models/item');
 
   await Item.deleteMany({});
   const items = await Item.create([
-    {name: 'Kawasaki Z1 - Rickman', emoji: 'https://ogden_images.s3.amazonaws.com/www.motorcycleclassics.com/images/2014/12/11085451/Rickman-Main-jpg.jpg', category: categories[0], price: 10.00},
-    {name: 'Kawasaki ZX10R - EVA 01', emoji: 'https://i.ytimg.com/vi/o7lC9cq7gno/maxresdefault.jpg', category: categories[0], price: 10.00},
-    {name: 'Ducati MH900 Evoluzione', emoji: 'img/DucatiMh900E.jpg', category: categories[1], price: 10.00},
-    {name: 'Ducati Paul Smart 1100', emoji: 'https://upload.wikimedia.org/wikipedia/commons/3/39/PaulSmartLE1000.jpg', category: categories[1], price: 10.00},
+    {name: 'Kawasaki Z1 - Rickman', image: 'https://ogden_images.s3.amazonaws.com/www.motorcycleclassics.com/images/2014/12/11085451/Rickman-Main-jpg.jpg', category: categories[0], price: 10.00},
+    {name: 'Kawasaki ZX10R - EVA 01', image: 'https://i.ytimg.com/vi/o7lC9cq7gno/maxresdefault.jpg', category: categories[0], price: 10.00},
+    {name: 'Ducati MH900 Evoluzione', image: 'https://upload.wikimedia.org/wikipedia/commons/6/68/Ducati_MHe.jpg', category: categories[1], price: 10.00},
+    {name: 'Ducati Paul Smart 1100', image: 'https://upload.wikimedia.org/wikipedia/commons/3/39/PaulSmartLE1000.jpg', category: categories[1], price: 10.00},
   ]);
 
   console.log(items)
